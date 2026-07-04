6958b60614f7777e990e4adbb7bc73b23b5fcc3f
Kuldeep-Tinker <kuldeeptinker1@gmail.com>
2026-07-04 23:20:23 +0530

    Add rain detection project files

---
+++ arduino-code/Rain_Detector_Code.ino
@@ -0,0 +1,51 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+//LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+  LiquidCrystal lcd(13, 12, 11, 10,  9,  8);
+ 
+const int SENSOR_PIN = 2;
+const int BUZZER_PIN = 7;
+const int LED_PIN = 6;
+ 
+void setup ()  
+{   
+  pinMode (SENSOR_PIN, INPUT); // Set the Sensor pin as INPUT  
+  pinMode(BUZZER_PIN, OUTPUT);  // Set the BUZZER pin as OUTPUT 
+  pinMode(LED_PIN, OUTPUT);  // Set the LED pin as OUTPUT 
+
+  lcd.begin(20, 4); // set up the LCD's number of columns and rows:
+  lcd.setCursor(0,0); // set the cursor position:
+  lcd.print("  THE BRIGHT LIGHT    ");
+  lcd.setCursor(0,1);
+  lcd.print("   RAIN DETECTOR             ");
+}  
+void loop ()  
+{
+  int Sensor_Val = digitalRead(SENSOR_PIN);  //get reading from Sensor
+  if (Sensor_Val == HIGH) //If Sensor Detected the Rain
+  {
+    digitalWrite(BUZZER_PIN, HIGH);
+    digitalWrite(LED_PIN, LOW);
+    
+    lcd.setCursor(0, 3);
+    lcd.print("      RAINING             ");
+    delay(100);
+    lcd.setCursor(0, 3);
+    lcd.print("      RAINING.             ");
+    delay(100);
+    lcd.setCursor(0, 3);
+    lcd.print("      RAINING..             ");
+    delay(100);
+    lcd.setCursor(0, 3);
+    lcd.print("      RAINING...             ");
+    delay(100);
+  }
+  else
+  {
+    lcd.setCursor(0, 3);
+    lcd.print("      NO RAIN             ");
+    digitalWrite(BUZZER_PIN, LOW);
+    digitalWrite(LED_PIN, HIGH);
+  }
+}
